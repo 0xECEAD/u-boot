6d2ba55b444869e7686dc2928ffced00bef7a400
Enceladus <enc@seynen.net>
2024-12-23 14:22:22 +0100

    set config to better match hardware (development phase) fix header include in vesa.c (write combinning) disable cpld command added 'amiga' default environment (bogus initial set) included SD-card image creation in run script

--- run.sh
+++ run.sh
@@ -1,7 +1,26 @@
 CROSS_COMPILE=powerpc-linux-gnu-
 export CROSS_COMPILE
 
-mkdir -p ~/mirari
-make O=~/mirari distclean
-make O=~/mirari Mirari_SDCARD_defconfig
-make O=~/mirari all -j 6
+OUTPATH=~/mirari-uboot
+IMGFILE=${OUTPATH}/Mirari.img
+
+mkdir -p ${OUTPATH}
+make O=${OUTPATH} distclean
+make O=${OUTPATH} Mirari_SDCARD_defconfig
+make O=${OUTPATH} all -j $(nproc)
+
+cat board/freescale/mirari/default_env_strings | sort | ${OUTPATH}/tools/mkenvimage -b -p 0 -s 8192 -o ${OUTPATH}/mirari-env -
+
+#
+# Create the image file for writting to the SD card.
+#
+# u-boot.pbl is at 8 blocks (0x1000 bytes)
+# default environment is at 1800 (0xE1000 bytes)
+# amigaboot is at 16384 blocks (0x800000 bytes)
+# data image blob is at 18432 blocks (0x900000 bytes)
+#
+rm -f ${IMGFILE}
+dd if=${OUTPATH}/u-boot-with-spl-pbl.bin of=${IMGFILE} bs=512 seek=8 conv=notrunc
+dd if=${OUTPATH}/mirari-env of=${IMGFILE} bs=512 seek=1800 conv=notrunc
+#dd if=amigaboot of=${IMGFILE} bs=512 seek=16384 conv=notrunc
+#dd if=dataimage of=${IMGFILE} bs=512 seek=18432 conv=notrunc
